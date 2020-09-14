152af2407c3882bd92a3452082ab10c152231c48
sayalikarnewar <sayali.karnewar@gmail.com>
2020-09-14 16:34:55 +0530

    commit to master

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tutorial(models.Model):
+    Tutorial_title = models.CharField(max_length = 200)
+    Tutorial_content = models.TextField()
+    Tutorial_published = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.Tutorial_title 
